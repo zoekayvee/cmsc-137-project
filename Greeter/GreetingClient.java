bbbd95ad58f42a9b9e2278c08f544e99d62efaf5
Zoe Villanueva <zoektv@gmail.com>
2017-11-08 00:20:24 +0800

    Sender receives message from server

--- Greeter/GreetingClient.java
+++ Greeter/GreetingClient.java
@@ -15,7 +15,6 @@ public class GreetingClient{
             try{
                 String serverName = args[0]; //get IP address of server from first param
                 int port = Integer.parseInt(args[1]); //get port from second param
-                String message = args[2]; //get message from the third param
 
                 /* Open a ClientSocket and connect to ServerSocket */
                 System.out.println("Connecting to " + serverName + " on port " + port);
@@ -24,33 +23,55 @@ public class GreetingClient{
                 Socket server = new Socket(serverName, port);
 
                 System.out.println("Just connected to " + server.getRemoteSocketAddress());
+                
+                Thread sender = new Thread() {
+                    public void run() {
+                        Scanner scanIn = new Scanner(System.in);
+                        try{
+                            while(connected) {            
+                                /* Send data to the ServerSocket */
+                                OutputStream outToServer = server.getOutputStream();
+                                DataOutputStream out = new DataOutputStream(outToServer);
 
-                Scanner scanIn = new Scanner(System.in);
-                while(connected) {            
-        			/* Send data to the ServerSocket */
-                    OutputStream outToServer = server.getOutputStream();
-                    DataOutputStream out = new DataOutputStream(outToServer);
+                                System.out.println("Enter something: ");
+                                String msg;
 
-                    System.out.println("Enter something: ");
-                    String msg;
+                                msg = scanIn.next();
 
-                    msg = scanIn.next();
+                                out.writeUTF("From client " + server.getLocalSocketAddress()+" says: " + msg);
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                        scanIn.close();
+                    }
+                };
 
-                    out.writeUTF("From client " + server.getLocalSocketAddress()+" says: " + msg);
+                Thread receiver = new Thread() {
+                    public void run() {
+                        try {
+                            System.out.println("Will be receiving messages from server");
+                            while (connected) {
+                                InputStream inFromServer = server.getInputStream();
+                                DataInputStream in = new DataInputStream(inFromServer);
+                                System.out.println("Server says " + in.readUTF());
+                            }
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                };
 
-                    /* Receive data from the ServerSocket */
-                    InputStream inFromServer = server.getInputStream();
-                    DataInputStream in = new DataInputStream(inFromServer);
-                    System.out.println("Server says " + in.readUTF());
-                }
-                scanIn.close();
+                sender.start();
+                receiver.start();
+
     			//closing the socket of the client
                 //server.close();
             }catch(IOException e){
                 e.printStackTrace();
                 System.out.println("Cannot find (or disconnected from) Server");
             }catch(ArrayIndexOutOfBoundsException e){
-                System.out.println("Usage: java GreetingClient <server ip> <port no.> '<your message to the server>'");
+                System.out.println("Usage: java GreetingClient <server ip> <port no.>");
             }
     }
 }
